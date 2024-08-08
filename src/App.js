e34e51bdf1925bb0f4f9b7497f02540412678578
Yasmin <vizeugilyasmin@gmail.com>
2024-08-08 15:07:43 -0300

    creating component container with prop children

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
-
+import Banner from "./components/Banner"
+import Container from "./components/Container";
 
 const App = () => {
   return (
    <>
-   <Header/>
-   <h1>Hello world!</h1>
-   <p>Ola mundo estou aprendedo react js</p>
-   <Footer></Footer>
+   <Header />
+   <Banner />
+   <Container> 
+    <h1>Hello world!</h1>
+    <p>Ola mundo estou aprendedo react js</p>
+    </Container>
+   <Footer/>
    </>
   );
 }
